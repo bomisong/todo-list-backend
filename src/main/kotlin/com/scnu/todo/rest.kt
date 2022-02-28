35a8225adbe5a0a715cf21b0c71ab21a159bd581
bomisong <a01046756465@gmail.com>
2022-02-28 22:26:10 +0900

    test commit

---
+++ src/main/kotlin/com/scnu/todo/rest.kt
@@ -0,0 +1,4 @@
+package com.scnu.todo
+
+class rest {
+}
--- src/main/kotlin/com/scnu/todo/web/rest/TodoResource.kt
+++ src/main/kotlin/com/scnu/todo/web/rest/TodoResource.kt
@@ -1,11 +1,18 @@
 package com.scnu.todo.web.rest
 
 import com.scnu.todo.domain.Todo
-import com.scnu.todo.repository.TodoRepository
+import com.scnu.todo.service.TodoService
 import com.scnu.todo.service.dto.TodoDTO
-import com.scnu.todo.service.mapper.TodoMapper
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.Valid
@@ -13,28 +20,27 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/api")
 class TodoResource(
-    private val todoRepository: TodoRepository,
-    private val todoMapper: TodoMapper,
+    private val todoService: TodoService,
 ) {
 
     @PostMapping("/todos")
     fun createTodo(@Valid @RequestBody todoDTO: TodoDTO): Mono<Todo> {
-        return todoRepository.save(todoMapper.toEntity(todoDTO))
+        return todoService.createTodo(todoDTO)
     }
 
     @PutMapping("/todos")
     fun updateTodo(@Valid @RequestBody todoDTO: TodoDTO): Mono<Todo> {
-        return todoRepository.save(todoMapper.toEntity(todoDTO))
+        return todoService.updateTodo(todoDTO)
     }
 
     @GetMapping("/todos")
     fun getAllTodos(): Flux<TodoDTO> {
-        return todoRepository.findAll().map(todoMapper::toDto)
+        return todoService.getAllTodos()
     }
 
     @DeleteMapping("/todos/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     fun deleteTodo(@PathVariable id: String): Mono<Void> {
-        return todoRepository.deleteById(id)
+        return todoService.deleteTodo(id)
     }
 }
